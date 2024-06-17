5ae56e00903eb140d1481ee123f22152c63f22d4
Darth_Zapp <57848725+darthzapp@users.noreply.github.com>
2024-06-17 23:50:02 +0200

    Update startup.lua

--- startup.lua
+++ startup.lua
@@ -1,9 +1,14 @@
 monitor = peripheral.find("monitor")
 
+function download_images()
+    shell.run("wget https://raw.githubusercontent.com/darthzapp/Minecraft-SGJurney-CC-Dialing-Program/main/sg_idle.nfp sg_idle.nfp")
+end
+
 function draw_image()
     imagepath = "sg_idle.nfp"
     image = paintutils.loadImage(imagepath)
     paintutils.drawImage(image,1,1)
 end
 
+download_images()
 draw_image()
