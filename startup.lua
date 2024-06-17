7c81ce95abf88cda960fe11b87c974280493a274
Darth_Zapp <57848725+darthzapp@users.noreply.github.com>
2024-06-18 00:01:05 +0200

    Update startup.lua

--- startup.lua
+++ startup.lua
@@ -1,15 +1,47 @@
 monitor = peripheral.find("monitor")
 
 function download_images()
-    shell.run("wget https://raw.githubusercontent.com/darthzapp/Minecraft-SGJurney-CC-Dialing-Program/main/sg_idle.nfp sg_idle.nfp")
-    print("[info] downloaded sg_idle.nfp ")
+    shell.run(" ")
+    print(" ")
 end
 
-function draw_image()
-    imagepath = "sg_idle.nfp"
-    image = paintutils.loadImage(imagepath)
-    paintutils.drawImage(image,1,1)
+function setPixel(x, y, color)
+    term.setCursorPos(x, y)
+    term.setBackgroundColor(color)
+    term.write(" ")
 end
 
-download_images()
-draw_image()
+
+function drawCircle(centerX, centerY, radius, color)
+    local x = radius
+    local y = 0
+    local decisionOver2 = 1 - x  -- Entscheidungskriterium
+
+    while y <= x do
+        setPixel(centerX + x, centerY + y, color)
+        setPixel(centerX + y, centerY + x, color)
+        setPixel(centerX - x, centerY + y, color)
+        setPixel(centerX - y, centerY + x, color)
+        setPixel(centerX - x, centerY - y, color)
+        setPixel(centerX - y, centerY - x, color)
+        setPixel(centerX + x, centerY - y, color)
+        setPixel(centerX + y, centerY - x, color)
+        y = y + 1
+
+        if decisionOver2 <= 0 then
+            decisionOver2 = decisionOver2 + 2 * y + 1  -- Ändere Entscheidungskriterium
+        else
+            x = x - 1
+            decisionOver2 = decisionOver2 + 2 * (y - x) + 1  -- Ändere Entscheidungskriterium
+        end
+    end
+end
+
+term.clear()
+local centerX, centerY = term.getSize()
+centerX = math.floor(centerX / 2)
+centerY = math.floor(centerY / 2)
+local radius = 10
+local color = colors.red
+
+drawCircle(centerX, centerY, radius, color)
