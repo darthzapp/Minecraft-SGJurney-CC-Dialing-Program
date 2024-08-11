1a6476de172719ea96fa114609055907dad81193
Darth_Zapp <57848725+darthzapp@users.noreply.github.com>
2024-08-11 21:12:10 +0200

    Update dialingprogram_v2_1.lua

--- dialingprogram_v2_1.lua
+++ dialingprogram_v2_1.lua
@@ -300,7 +300,7 @@ while true do
         else
             if interface.isStargateConnected() then
                 interface.disconnectStargate()
-            elseif getChevronsEngaged() > 0 then
+            elseif interface.getChevronsEngaged() > 0 then
                 interface.disconnectStargate()
             else
                 print("Destination: " .. label)
